8dc05f63cdea6dd16df7499b731e5562a4358e7e
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-19 10:02:37 -0300

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 const App = (props) => {
+  const [contar, setContar] = React.useState(0);
+  console.log('exeutou fora')
+  React.useEffect(() => console.log('executou'), []);
+
+  React.useEffect(() => {
+    document.title = 'Total ' + contar
+  }, [contar])
+
   return (
     <div>
-      <button>teste</button>
+      <button onClick={() => setContar(contar + 1)}>{contar}</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
