95048b63c2a042940d1f52762a692e3316622fb0
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-19 14:26:57 -0300

    ex 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const App = (props) => {
-  const [coment, setComent] = React.useState(['teste', 'teste2', 'oi', 'ok']);
+  const [coment, setComent] = React.useState([]);
+  const [input, setInput] = React.useState('');
+  const inputElement = React.useRef();
+
+  function handleClick() {
+    setComent([...coment, input]);
+    setInput('');
+    inputElement.current.focus();
+  }
+
   return (
     <div>
       <ul>
@@ -9,6 +18,15 @@ const App = (props) => {
           <li key={item}>{item}</li>
         ))}
       </ul>
+      <input
+        type="text"
+        ref={inputElement}
+        value={input}
+        onChange={({ target }) => setInput(target.value)}
+      />
+      <button onClick={handleClick} style={{ marginTop: '1rem' }}>
+        Enviar
+      </button>
     </div>
   );
 };
