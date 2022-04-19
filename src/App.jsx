da03db2f3ef59a056d5fbf391d7b7a495cd0b3ae
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-19 14:52:45 -0300

    ex 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const App = (props) => {
+  const [contar, setContar] = React.useState(0);
+  const valor = React.useMemo(() => {
+    const localItem = window.localStorage.getItem('produto');
+    return localItem;
+  }, []);
+  console.log(valor);
   return (
     <div>
-      
+      <button onClick={() => setContar(contar + 1)}>Click {contar}</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
