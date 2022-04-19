433f8b1c9dfb51cff7648b19c5ca05399228518f
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-19 14:57:43 -0300

    ex2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 const App = (props) => {
   const [contar, setContar] = React.useState(0);
-  const valor = React.useMemo(() => {
-    const localItem = window.localStorage.getItem('produto');
-    return localItem;
-  }, []);
-  console.log(valor);
+  const t1 = performance.now();
+  const valor = React.useMemo(() => operacaoLenta(), []);
+  console.log(performance.now() - t1);
+
+  function operacaoLenta() {
+    let c;
+    for (let i = 0; i < 100000000; i++) {
+      c = i + 1 / 10;
+    }
+    return c;
+  }
+
   return (
     <div>
       <button onClick={() => setContar(contar + 1)}>Click {contar}</button>
