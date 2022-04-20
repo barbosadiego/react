962c65c75a1f32563a76388047596f24a0937f96
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-20 14:35:18 -0300

    ex antes correcao

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
 import React from 'react';
 
-const App = (props) => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
+const App = () => {
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
+
+  const [confirm, setConfirm] = React.useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    // console.log(form);
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((res) => setConfirm(res));
+    // .then((json) => setConfirm(json));
+    console.log(confirm);
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="nome">Nome</label>
-      <input
-        id="nome"
-        type="text"
-        value={nome}
-        onChange={({ target }) => setNome(target.value)}
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        id="email"
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-      />
+      {Object.keys(form).map((key) => (
+        <div key={key}>
+          <label htmlFor={key} style={{ textTransform: 'capitalize' }}>
+            {key}
+          </label>
+          <input
+            id={key}
+            type="text"
+            value={form[key]}
+            onChange={handleChange}
+          />
+        </div>
+      ))}
       <button>Enviar</button>
+      {confirm && confirm.status === 200 ? 'Usuário cadastrado' : ''}
     </form>
   );
 };
