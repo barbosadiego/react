121a5305f2057c2a0e5d6219d79f61ebdffd2b96
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-20 11:20:43 -0300

    form 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 
 const App = (props) => {
   const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
       <input
+        id="nome"
         type="text"
         value={nome}
         onChange={({ target }) => setNome(target.value)}
       />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <button>Enviar</button>
     </form>
   );
 };
