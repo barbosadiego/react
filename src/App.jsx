023d5b75fcca2dc415d5217a1bf5a2440a3d4c1b
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-20 11:01:05 -0300

    use-fetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
-import useLocalStorage from './useLocalStorage'
+import React from 'react';
+import useFetch from './useFetch';
+import useLocalStorage from './useLocalStorage';
 
 const App = (props) => {
-  const [produto, setProduto] = useLocalStorage('produto', '')
+  const [produto, setProduto] = useLocalStorage('produto', '');
+  const { request, data, loading, error } = useFetch();
 
-  function handleClick({target}){
-    setProduto(target.innerText)
+  function handleClick({ target }) {
+    setProduto(target.innerText);
   }
 
-  return (
-    <div>
-      <button onClick={handleClick} style={{marginRight:'1rem'}}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-    </div>
-  )
-}
+  React.useEffect(() => {
+    async function fetchData() {
+      const obj = await request(
+        'https://ranekapi.origamid.dev/json/api/produto/',
+      );
+      // console.log(obj)
+    };
+    fetchData()
+  }, [request]);
 
-export default App
+  if (error) return <p>{error}</p>;
+  if (loading) return <h1>Carregando ...</h1>;
+  if (data)
+    return (
+      <div>
+        <button onClick={handleClick} style={{ marginRight: '1rem' }}>
+          notebook
+        </button>
+        <button onClick={handleClick}>smartphone</button>
+
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.nome} / R$ {item.preco}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  else return null;
+};
+
+export default App;
---
+++ src/useFetch.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const set = new Set()
+
+const useFetch = (props) => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json
+    try {
+      setError(null)
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+    } catch (error) {
+      setError('Ocorreu um erro');
+      json = null;
+    } finally {
+      setData(json);
+      setLoading(false);
+      return {response, json}
+    }
+  }, [])
+
+  set.add(request)
+  console.log(set)
+  return { data, error, loading, request };
+};
+
+
+export default useFetch;
