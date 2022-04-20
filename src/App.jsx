4c3842d6606003d36f6d0ab17b18e06d444b0e62
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-20 15:39:30 -0300

    radio-itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,50 @@
 import React from 'react';
-import Button from './Button';
-import Produto from './Produto';
 
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
+const App = (props) => {
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
 
-const App = () => {
-  const [item, setItem] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
+  function handleChange({ target }) {
+    setProduto(target.value);
+  }
 
-  async function handleFetch(e) {
-    setLoading(true);
-    const produto = e.target.innerText;
-    const res = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${produto}`,
-    );
-    const json = await res.json();
-    setItem(json);
-    
-    setLoading(false);
+  function handleCor({ target }) {
+    setCor(target.value);
   }
 
   return (
-    <>
-      <Button onClick={handleFetch} texto="notebook" />
-      <Button onClick={handleFetch} texto="smartphone" />
-      <Button onClick={handleFetch} texto="tablet" />
-      {item && !loading ? <Produto dados={item} /> : null}
-      {loading && <p>Carregando...</p>}
-    </>
+    <form>
+      <h2>Cores</h2>
+      <label>
+        <input type="radio" name="cor" onChange={handleCor} value="azul" />
+        Azul
+      </label>
+      <label>
+        <input type="radio" name="cor" onChange={handleCor} value="amarelo" />
+        Amarelo
+      </label>
+      <p>Selecionado: {cor}</p>
+      <h2>Produtos</h2>
+      <label>
+        <input
+          type="radio"
+          name="produto"
+          onChange={handleChange}
+          value="smartphone"
+        />
+        Smartphone
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="produto"
+          onChange={handleChange}
+          value="notebook"
+        />
+        Notebook
+      </label>
+      <p>Selecionado: {produto}</p>
+    </form>
   );
 };
 
