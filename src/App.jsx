37ab4c6836e69a35ba29b2d9f7d053477eba969d
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-21 08:56:26 -0300

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import Button from './Button';
-import Produto from './Produto';
-
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
+import Input from './Form/Input';
 
 const App = () => {
-  const [item, setItem] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
-
-  async function handleFetch(e) {
-    setLoading(true);
-    const produto = e.target.innerText;
-    const res = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${produto}`,
-    );
-    const json = await res.json();
-    setItem(json);
-    
-    setLoading(false);
-  }
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
 
   return (
-    <>
-      <Button onClick={handleFetch} texto="notebook" />
-      <Button onClick={handleFetch} texto="smartphone" />
-      <Button onClick={handleFetch} texto="tablet" />
-      {item && !loading ? <Produto dados={item} /> : null}
-      {loading && <p>Carregando...</p>}
-    </>
+    <form>
+      <Input id="nome" label="nome" value={nome} setValue={setNome} />
+      <Input
+        id="email"
+        label="email"
+        value={email}
+        setValue={setEmail}
+        requeried
+      />
+      <button>enviar</button>
+    </form>
   );
 };
 
---
+++ src/Form/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Input = ({ label, id, setValue, requeried, ...props }) => {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type="text"
+        id={id}
+        name={id}
+        onChange={({ target }) => setValue(target.value)}
+        required={requeried}
+        {...props}
+      />
+    </>
+  );
+};
+
+export default Input;
