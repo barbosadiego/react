e26aa8cdf9bde1d429e02f5581ac673f16cff675
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-21 09:16:45 -0300

    select

---
+++ src/Form/Select.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Select = ({ options, value, setValue, ...props }) => {
+  return (
+    <select
+      value={value}
+      onChange={({ target }) => setValue(target.value)}
+      {...props}
+    >
+      <option value="" disabled>
+        Selecione
+      </option>
+      {options.map((item) => (
+        <option key={item} value={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
