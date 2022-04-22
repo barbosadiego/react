11f69ff89fe6cbf613d8d46590a7ef28b1fc32d9
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-22 10:58:15 -0300

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import './App.css'
-import Produto from './Components/Produto'
+import styled from 'styled-components'
+
+const Titulo = styled.h1`
+  font-size: 2.4rem;
+  color: #43c;
+`
+
+const Paragrafo = styled.p`
+  font-size: 1.4rem;
+  line-height: 1.2;
+  color: tomato;
+`
 
 const App = (props) => {
   return (
     <div>
-      <Produto />
+      <Titulo>Meu título</Titulo>
+      <Paragrafo>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse nemo cum reprehenderit inventore aspernatur. Numquam, cupiditate! Officia ipsa nulla enim adipisci ex laudantium fugiat vero expedita dignissimos ipsum, quidem eveniet?</Paragrafo>
     </div>
   )
 }
