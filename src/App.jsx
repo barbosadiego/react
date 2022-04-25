594e3edfb73341cf49a6cef5196885457eda55fb
barbosadiego <diego.gutemberg@hotmail.com>
2022-04-25 10:00:25 -0300

    aula react reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,37 @@
 import React from 'react';
-import Button from './Button';
-import Produto from './Produto';
+import Exemplo from './Exemplo';
 
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
-
-const App = () => {
-  const [item, setItem] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
-
-  async function handleFetch(e) {
-    setLoading(true);
-    const produto = e.target.innerText;
-    const res = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${produto}`,
-    );
-    const json = await res.json();
-    setItem(json);
-    
-    setLoading(false);
+function reducer(state, action) {
+  // console.log(state);
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      throw new Error();
   }
+}
+
+const App = (props) => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
 
   return (
-    <>
-      <Button onClick={handleFetch} texto="notebook" />
-      <Button onClick={handleFetch} texto="smartphone" />
-      <Button onClick={handleFetch} texto="tablet" />
-      {item && !loading ? <Produto dados={item} /> : null}
-      {loading && <p>Carregando...</p>}
-    </>
+    <div>
+      <div>
+        <p>{state}</p>
+        <button
+          style={{ marginRight: '1rem' }}
+          onClick={() => dispatch('aumentar')}
+        >
+          +
+        </button>
+        <button onClick={() => dispatch('diminuir')}>-</button>
+      </div>
+      <div>
+        <Exemplo/>
+      </div>
+    </div>
   );
 };
 
